66ad60588bf487b6235845b08864eebab670828b
Gaetz <gaetz@rufflerim.com>
2017-05-02 17:38:29 +0200

    Add music and solve a bug when restarting game

--- Assets/_Code/MusicPlayer.cs
+++ Assets/_Code/MusicPlayer.cs
@@ -3,15 +3,48 @@ using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
 	static MusicPlayer instance = null;
-	
-	void Start () {
+
+    public AudioClip StartClip;
+    public AudioClip GameClip;
+    public AudioClip EndClip;
+
+    private AudioSource music;
+
+    void Start () {
 		if (instance != null && instance != this) {
 			Destroy (gameObject);
 			print ("Duplicate music player self-destructing!");
-		} else {
+        }
+        else
+        {
 			instance = this;
 			GameObject.DontDestroyOnLoad(gameObject);
-		}
-		
-	}
+            // Start first music
+            music = GetComponent<AudioSource>();
+            music.clip = StartClip;
+            music.loop = true;
+            music.Play();
+        }
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        if(music == null)
+            music = GetComponent<AudioSource>();
+        music.Stop();
+        switch(level)
+        {
+            case 0:
+                music.clip = StartClip;
+                break;
+            case 1:
+                music.clip = GameClip;
+                break;
+            case 2:
+                music.clip = EndClip;
+                break;
+        }
+        music.loop = true;
+        music.Play();
+    }
 }
