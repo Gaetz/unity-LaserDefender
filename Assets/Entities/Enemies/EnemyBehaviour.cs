8b690ff4f942b6b11f79048f111393c98e7dbaf7
Gaetz <gaetz@rufflerim.com>
2017-05-02 12:40:59 +0200

    Player shoots lasers that destroy enemies

---
+++ Assets/Entities/Enemies/EnemyBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    public float Health;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        // Handle projectile hit
+        Projectile projectile = collider.gameObject.GetComponent<Projectile>();
+        if(projectile)
+        {
+            Health -= projectile.Damage;
+            projectile.Hit();
+        }
+        // Death
+        if(Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Entities/Laser/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float Damage;
+
+    public void Hit()
+    {
+        Destroy(gameObject);
+    }
+}
